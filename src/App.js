525a31fc61429fedf1582c5df4a224c118d85f1f
Timo Staudinger <mail@timostaudinger.com>
2020-02-18 15:39:36 -0500

    Add hue selector

--- src/App.js
+++ src/App.js
@@ -1,70 +1,25 @@
-import React, { useRef, useEffect } from 'react';
-import './App.css';
+import React, {useState} from 'react'
 
-function hslToRGB(h,s,l) {
-  let c = (1 - Math.abs(2 * l - 1)) * s,
-      x = c * (1 - Math.abs((h / 60) % 2 - 1)),
-      m = l - c/2,
-      r = 0,
-      g = 0,
-      b = 0;
+import ColorPicker from './colorpicker/ColorPicker'
 
-  if (0 <= h && h < 60) {
-    r = c; g = x; b = 0;
-  } else if (60 <= h && h < 120) {
-    r = x; g = c; b = 0;
-  } else if (120 <= h && h < 180) {
-    r = 0; g = c; b = x;
-  } else if (180 <= h && h < 240) {
-    r = 0; g = x; b = c;
-  } else if (240 <= h && h < 300) {
-    r = x; g = 0; b = c;
-  } else if (300 <= h && h < 360) {
-    r = c; g = 0; b = x;
-  }
-  r = Math.round((r + m) * 255);
-  g = Math.round((g + m) * 255);
-  b = Math.round((b + m) * 255);
+import './App.css'
 
-  return [r, g, b];
-}
-
-const coordsToOffset = (x, y, width) => (x + y * width) * 4
-
-function App() {
-  const canvas = useRef(null)
-
-  useEffect(() => {
-    let context = canvas.current.getContext('2d')
-    let imageData = context.getImageData(0, 0, 200, 200)
-
-    console.log('running', imageData)
-    
-    for (let y = 0; y < canvas.current.height; y++) {
-    for(let x = 0; x < canvas.current.width; x++) {
-      const pixelOffset = coordsToOffset(x, y, canvas.current.width)
-
-      let hue = 250
-      let saturation = x / canvas.current.width
-      let lightness =  1 - (y / canvas.current.height)
-
-      let [r, g, b] = hslToRGB(hue, saturation, lightness)
-
-      imageData.data[pixelOffset] = r;
-      imageData.data[pixelOffset + 1] = g;
-      imageData.data[pixelOffset + 2] = b;
-      imageData.data[pixelOffset + 3] = 255;
-    }}
-    console.log(imageData)
-
-    context.putImageData(imageData, 0, 0)
-  }, [canvas])
+const App = () => {
+  const [hue, setHue] = useState(100)
 
   return (
     <div className="App">
-      <canvas height={200} width={200} ref={canvas}>Fallback</canvas>
+      <input
+        type="number"
+        min={0}
+        max={359}
+        value={hue}
+        onChange={e => setHue(e.target.value)}
+      />
+
+      <ColorPicker hue={hue} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/colorpicker/ColorPicker.js
@@ -0,0 +1,79 @@
+import React, {useRef, useEffect} from 'react'
+
+function convertHSLtoRGB(h, s, l) {
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+    m = l - c / 2,
+    r = 0,
+    g = 0,
+    b = 0
+
+  if (0 <= h && h < 60) {
+    r = c
+    g = x
+    b = 0
+  } else if (60 <= h && h < 120) {
+    r = x
+    g = c
+    b = 0
+  } else if (120 <= h && h < 180) {
+    r = 0
+    g = c
+    b = x
+  } else if (180 <= h && h < 240) {
+    r = 0
+    g = x
+    b = c
+  } else if (240 <= h && h < 300) {
+    r = x
+    g = 0
+    b = c
+  } else if (300 <= h && h < 360) {
+    r = c
+    g = 0
+    b = x
+  }
+  r = Math.round((r + m) * 255)
+  g = Math.round((g + m) * 255)
+  b = Math.round((b + m) * 255)
+
+  return [r, g, b]
+}
+
+const convertCoordsToOffset = (x, y, width) => (x + y * width) * 4
+
+const ColorPicker = ({hue = 250, width = 200, height = 200}) => {
+  const canvas = useRef(null)
+
+  useEffect(() => {
+    if (canvas.current) {
+      let width = canvas.current.width
+      let height = canvas.current.height
+
+      let context = canvas.current.getContext('2d')
+      let imageData = context.getImageData(0, 0, width, height)
+
+      for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+          const pixelOffset = convertCoordsToOffset(x, y, width)
+
+          let saturation = x / width
+          let lightness = 1 - y / height
+
+          let [r, g, b] = convertHSLtoRGB(hue, saturation, lightness)
+
+          imageData.data[pixelOffset + 0] = r
+          imageData.data[pixelOffset + 1] = g
+          imageData.data[pixelOffset + 2] = b
+          imageData.data[pixelOffset + 3] = 255
+        }
+      }
+
+      context.putImageData(imageData, 0, 0)
+    }
+  }, [canvas, hue])
+
+  return <canvas height={height} width={width} ref={canvas}></canvas>
+}
+
+export default ColorPicker
