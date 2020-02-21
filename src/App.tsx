67a912b12ba832074db36aa2be95a357f950bcf0
Timo Staudinger <mail@timostaudinger.com>
2020-02-21 18:06:55 -0500

    Add combined color picker component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 import './App.css'
 import ColorPicker from './pickers/ColorPicker'
-import HuePicker from './pickers/HuePicker'
 
 const App = () => {
-  const [hue, setHue] = useState(250)
-  const [saturation, setSaturation] = useState(0.5)
-  const [lightness, setLightness] = useState(0.5)
-
   return (
     <div className="App">
-      <HuePicker hue={hue} setHue={setHue} />
-
-      <ColorPicker
-        hue={hue}
-        saturation={saturation}
-        lightness={lightness}
-        setSaturation={setSaturation}
-        setLightness={setLightness}
-      />
+      <ColorPicker />
     </div>
   )
 }
---
+++ src/pickers/ShadePicker.tsx
@@ -0,0 +1,136 @@
+import React, {useRef, useEffect, useState} from 'react'
+import {convertCoordsToOffset} from '../util/canvas'
+import {convertHSLtoRGB} from '../util/color'
+
+import styles from './ShadePicker.module.css'
+
+const canvasWidth = 200
+const canvasHeight = 200
+
+interface Props {
+  hue: number | null
+  saturation: number | null
+  lightness: number | null
+  setSaturation: (saturation: number) => void
+  setLightness: (lightness: number) => void
+}
+
+const ShadePicker = ({
+  hue = null,
+  saturation = null,
+  lightness = null,
+  setSaturation,
+  setLightness
+}: Props): JSX.Element => {
+  const [isDragging, setIsDragging] = useState(false)
+
+  const canvas = useRef<HTMLCanvasElement | null>(null)
+
+  const render = () => {
+    if (canvas.current !== null) {
+      let context = canvas.current.getContext('2d')
+
+      let {width, height} = canvas.current
+
+      if (context !== null) {
+        context.clearRect(0, 0, width, height)
+
+        let imageData = context.getImageData(0, 0, width, height)
+
+        if (hue !== null) {
+          for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+              const pixelOffset = convertCoordsToOffset(x, y, width)
+
+              let localSaturation = x / width
+              let localLightness = 1 - y / height
+
+              let [r, g, b] = convertHSLtoRGB(
+                hue,
+                localSaturation,
+                localLightness
+              )
+
+              imageData.data[pixelOffset + 0] = r
+              imageData.data[pixelOffset + 1] = g
+              imageData.data[pixelOffset + 2] = b
+              imageData.data[pixelOffset + 3] = 255
+            }
+          }
+
+          context.putImageData(imageData, 0, 0)
+
+          if (saturation !== null && lightness !== null) {
+            let x = saturation * width
+            let y = lightness * height
+
+            context.beginPath()
+            context.lineWidth = 1
+            context.strokeStyle = lightness < 0.5 ? '#111' : '#fff'
+            context.arc(x, y, 8, 0, Math.PI * 2, true)
+            context.stroke()
+          } else console.log('`saturation` or `lightness` is null')
+        } else console.log('`hue` is null')
+      } else console.log('`context` is null')
+    } else console.log('`canvas.current` is null')
+  }
+
+  const handleMouseMove = (
+    e: React.MouseEvent<HTMLCanvasElement, MouseEvent>
+  ) => {
+    if (canvas.current !== null) {
+      let {left, top} = canvas.current.getBoundingClientRect()
+
+      let x = e.clientX - left
+      let y = e.clientY - top
+
+      if (isDragging && canvas.current !== null) {
+        let {width, height} = canvas.current
+        setSaturation(x / width)
+        setLightness(y / height)
+      }
+    }
+  }
+  const handleMouseDown = (
+    e: React.MouseEvent<HTMLCanvasElement, MouseEvent>
+  ) => {
+    if (canvas.current !== null) {
+      let {width, height} = canvas.current
+      let {left, top} = canvas.current.getBoundingClientRect()
+
+      let x = e.clientX - left
+      let y = e.clientY - top
+
+      setSaturation(x / width)
+      setLightness(y / height)
+      setIsDragging(true)
+    }
+  }
+  const handleMouseUp = () => {
+    setIsDragging(false)
+  }
+
+  const handleMouseLeave = () => {
+    setIsDragging(false)
+  }
+
+  useEffect(() => {
+    if (canvas.current) render()
+  })
+
+  return (
+    <div className={styles.picker}>
+      <canvas
+        height={canvasHeight}
+        width={canvasWidth}
+        ref={canvas}
+        onMouseMove={handleMouseMove}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseLeave}
+      ></canvas>
+    </div>
+  )
+}
+
+export default ShadePicker
