f64bc9aca088cd2ab8ee1aac52267f8a2a895894
Timo Staudinger <mail@timostaudinger.com>
2020-02-21 22:30:55 -0500

    Warn about invalid hex codes

--- src/components/colorpicker/ColorInput.tsx
+++ src/components/colorpicker/ColorInput.tsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react'
 import styles from './ColorInput.module.css'
 import {convertHSLToHex, convertHexToHSL} from '../../util/color'
 import {HSL} from '../../types/color'
+import {AlertTriangle} from 'react-feather'
 
 interface Props {
   hsl: HSL
@@ -11,24 +12,35 @@ interface Props {
 
 const ColorInput = ({hsl, setHSL}: Props): JSX.Element => {
   const [buffer, setBuffer] = useState('')
+  const [isInvalid, setIsInvalid] = useState(false)
 
   useEffect(() => {
+    setIsInvalid(false)
     setBuffer(convertHSLToHex(hsl))
   }, [hsl])
 
   const handleChangeHex = (hex: string): void => {
     setBuffer(hex)
 
-    console.log(hex)
-
     try {
+      setIsInvalid(false)
       setHSL(convertHexToHSL(hex))
-    } catch (e) {}
+    } catch (e) {
+      setIsInvalid(true)
+    }
   }
 
   return (
     <div className={styles.container}>
-      <input value={buffer} onChange={e => handleChangeHex(e.target.value)} />
+      <input
+        value={buffer}
+        onChange={e => handleChangeHex(e.target.value)}
+        className={isInvalid ? styles.invalid : undefined}
+      />
+
+      <AlertTriangle
+        className={styles.warningIcon + (isInvalid ? ' ' + styles.invalid : '')}
+      />
     </div>
   )
 }
