82ba83f9743e92a0ca8589ab822c7126d7089cb1
Timo Staudinger <mail@timostaudinger.com>
2020-02-23 01:27:50 -0500

    Prepare drop zones

---
+++ src/components/preview/ColorSlot.tsx
@@ -0,0 +1,95 @@
+import React, {ReactNode, useState} from 'react'
+import {useDrop} from 'react-dnd'
+import classnames from 'classnames'
+
+import {Color, Shade, HSL} from '../../types/color'
+
+import styles from './ColorSlot.module.css'
+
+const hslToCSS = (hsl: HSL) =>
+  `hsl(${hsl.hue}, ${Math.round(hsl.saturation * 100)}%, ${Math.round(
+    hsl.lightness * 100
+  )}%)`
+
+const getColor = (colors: Color[], colorName: string, shadeName: string) =>
+  colors
+    .find(color => color.name === colorName)
+    ?.shades.find(shade => shade.name === shadeName) as Shade
+
+interface Props {
+  colors: Color[]
+  children: (color: string) => ReactNode
+  defaultColorName: [string, string]
+}
+
+const ColorSlot = ({defaultColorName, colors, children}: Props) => {
+  const [shade, setShade] = useState<Shade>(
+    getColor(colors, defaultColorName[0], defaultColorName[1])
+  )
+
+  const [{canDrop, isOver}, drop] = useDrop({
+    accept: 'color',
+    drop: () => ({name: 'Dustbin'}),
+    collect: monitor => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop()
+    })
+  })
+  const [
+    {canDrop: canDropBackground, isOver: isOverBackground},
+    dropBackground
+  ] = useDrop({
+    accept: 'color',
+    drop: () => ({name: 'Dustbin'}),
+    collect: monitor => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop()
+    })
+  })
+  const [{canDrop: canDropBorder, isOver: isOverBorder}, dropBorder] = useDrop({
+    accept: 'color',
+    drop: () => ({name: 'Dustbin'}),
+    collect: monitor => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop()
+    })
+  })
+  console.log(isOver, canDrop)
+
+  return (
+    <span
+      className={classnames(styles.potentiallyDroppableElement, {
+        [styles.droppableElement]: canDrop
+      })}
+      style={{}}
+      ref={drop}
+    >
+      {children(hslToCSS(shade.hsl))}
+
+      {canDrop && (isOver || isOverBackground || isOverBorder) && (
+        <span className={styles.attributeDropZones}>
+          <span
+            className={classnames(styles.attributeDropZone, {
+              [styles.dropping]: isOverBackground
+            })}
+            ref={dropBackground}
+            style={{}}
+          >
+            Background
+          </span>
+          <span
+            className={classnames(styles.attributeDropZone, {
+              [styles.dropping]: isOverBorder
+            })}
+            ref={dropBorder}
+            style={{}}
+          >
+            Border
+          </span>
+        </span>
+      )}
+    </span>
+  )
+}
+
+export default ColorSlot
