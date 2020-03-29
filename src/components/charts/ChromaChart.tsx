360a647333adf683285ec886091e644dbbf3dab1
Timo Staudinger <mail@timostaudinger.com>
2020-03-29 17:38:16 -0400

    Properly layout charts

--- src/components/charts/ChromaChart.tsx
+++ src/components/charts/ChromaChart.tsx
@@ -1,13 +1,9 @@
-import React, {useEffect} from 'react'
-import * as d3 from 'd3'
+import React, {useEffect, useState} from 'react'
 
-import {convertLabToHex, convertLabToRGBStrict} from '../../util/color'
+import {convertLabToRGBStrict} from '../../util/color'
 import {Shade, Lab} from '../../types/color'
 
-import styles from './LightnessChart.module.css'
-
-const height = 200
-const width = 400
+import Chart from './Chart'
 
 const radToDeg = (rad: number) => (rad / Math.PI) * 180 + (rad > 0 ? 0 : 360)
 const calculateHue = ({a, b}: Lab) => radToDeg(Math.atan2(b, a))
@@ -25,209 +21,47 @@ const applyChromaToLab = (lab: Lab, chroma: number) => {
   return {...lab, a, b}
 }
 
-const draw = (containerId: string, shades: Shade[]) => {
-  let container = document.querySelector(`#chart-chroma-${containerId}`)
-  if (container) container.innerHTML = ''
-
-  let xScale = d3
-    .scaleLinear()
-    .domain([-0.5, shades.length - 0.5])
-    .range([0, width])
-
-  let yScale = d3
-    .scaleLinear()
-    .domain([chromaMin, chromaMax])
-    .range([height, 0])
-
-  let svg = d3
-    .select(`#chart-chroma-${containerId}`)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height)
-
-  let defs = svg.append('defs')
-
-  let line = d3
-    .line()
-    .x((d, i) => xScale(i))
-    .y(d => yScale(d[1]))
-    .curve(d3.curveMonotoneX)
-
-  // svg
-  //   .append('g')
-  //   .attr('class', 'x axis')
-  //   .attr('transform', `translate(0, ${height})`)
-  //   .call(d3.axisBottom(xScale))
-
-  // svg
-  //   .append('g')
-  //   .attr('class', 'y axis')
-  //   .call(d3.axisLeft(yScale))
-
-  const linearGradient = defs
-    .append('linearGradient')
-    .attr('id', `${containerId}-linear-gradient`)
-    .attr('gradientTrasform', 'rotate(90)')
-  shades.forEach((shade, i) => {
-    linearGradient
-      .append('stop')
-      .attr('offset', `${(100 * i) / shades.length}%`)
-      .attr('stop-color', '#' + convertLabToHex(shade.lab))
-  })
-
-  svg
-    .append('path')
-    .datum(
-      shades.map((shade, i): [number, number] => [
-        i,
-        calculateChroma(shade.lab)
-      ])
-    )
-    .attr('stroke-width', 4)
-    .attr('fill', 'none')
-    .attr('stroke', `url(#${containerId}-linear-gradient)`)
-    .attr('class', 'line')
-    .attr('d', line)
-
-  svg
-    .selectAll('.dot')
-    .data(
-      shades.map((shade, i): [number, number] => [
-        i,
-        calculateChroma(shade.lab)
-      ])
-    )
-    .enter()
-    .append('circle')
-    .attr('class', 'dot-border')
-    .attr('fill', d => '#fff')
-    .attr('stroke', d => '#ccc')
-    .attr('cx', (d, i) => xScale(i))
-    .attr('cy', d => yScale(d[1]))
-    .attr('r', 5)
-
-  svg
-    .selectAll('.dot')
-    .data(
-      shades.map((shade, i): [number, number] => [
-        i,
-        calculateChroma(shade.lab)
-      ])
-    )
-    .enter()
-    .append('circle')
-    .attr('class', 'dot-main')
-    .attr('fill', d => '#' + convertLabToHex(shades[d[0]].lab))
-    .attr('stroke', d => '#fff')
-    .attr('cx', (d, i) => xScale(i))
-    .attr('cy', d => yScale(d[1]))
-    .attr('r', 4)
-  // .on('mouseover', function(a, b, c) {
-  //   console.log(a)
-  //   this.attr('class', 'focus')
-  // })
-
-  // svg
-  // .append('g')
-  // .attr('class', 'axislabels')
-  // .attr('style', 'transform: translateY(0)')
-  // .call(d3.axisBottom(xScale).tickFormat(d => String(d)))
-  // .call(g => {
-  //   g.selectAll('.domain').remove()
-  //   g.selectAll('.tick line').remove()
-  //   g.selectAll('.tick').attr('style')
-  // })
-
-  const calculateBadAreasMin = (shades: Shade[], fixed: 'l' | 'c' | 'h') => {
-    let badAreas = shades.map(shade => {
-      for (let c = chromaMin; c <= chromaMax; c++) {
-        let rgb = convertLabToRGBStrict(applyChromaToLab(shade.lab, c))
-        if (rgb === null) continue
-
-        return {min: chromaMin, max: c - 1}
-      }
-
-      return {min: chromaMin, max: chromaMin}
-    })
-
-    return badAreas
-  }
-  const calculateBadAreasMax = (shades: Shade[], fixed: 'l' | 'c' | 'h') => {
-    let badAreas = shades.map(shade => {
-      for (let c = chromaMax; c >= chromaMin; c--) {
-        let rgb = convertLabToRGBStrict(applyChromaToLab(shade.lab, c))
-        if (rgb === null) continue
-
-        return {min: c + 1, max: chromaMax}
-      }
-
-      return {min: chromaMax, max: chromaMax}
-    })
-
-    console.log(badAreas)
-
-    return badAreas
-  }
-
-  let badAreasMin = calculateBadAreasMin(shades, 'c')
-  let badAreasMax = calculateBadAreasMax(shades, 'c')
-
-  console.log(badAreasMin, badAreasMax)
-
-  const areaMin = d3
-    .area<{min: number; max: number}>()
-    .x((value, i) => xScale(i))
-    .y0(value => yScale(value.min))
-    .y1(value => yScale(value.max))
-    .curve(d3.curveCardinal)
-  svg
-    .append('path')
-    .attr('fill', `url(#${containerId}-diagonal-stripes)`)
-    .attr('stroke', '#c1ccd9')
-    .attr('opacity', 0.5)
-    .attr('class', 'pointer-events-none')
-    // @ts-ignore
-    .attr('d', areaMin(badAreasMin))
-
-  const areaMax = d3
-    .area<{min: number; max: number}>()
-    .x((value, i) => xScale(i))
-    .y0(value => yScale(value.min))
-    .y1(value => yScale(value.max))
-    .curve(d3.curveCardinal)
-  svg
-    .append('path')
-    .attr('fill', `url(#${containerId}-diagonal-stripes)`)
-    .attr('stroke', '#c1ccd9')
-    .attr('opacity', 0.5)
-    .attr('class', 'pointer-events-none')
-    // @ts-ignore
-    .attr('d', areaMax(badAreasMax))
-}
-
 interface Props {
   id: string
   shades: Shade[]
+  setLab: (shadeId: string, lab: Lab) => void
 }
 
-const ChromaChart = ({id, shades}: Props) => {
+const ChromaChart = ({id, shades, setLab}: Props) => {
+  let [badAreas, setBadAreas] = useState<{min: number; max: number}[][]>([])
+
   useEffect(() => {
-    draw(id, shades)
+    const calculateBadAreasMax = (shades: Shade[]) => {
+      let badAreas = shades.map(shade => {
+        for (let c = chromaMax; c >= chromaMin; c--) {
+          let rgb = convertLabToRGBStrict(applyChromaToLab(shade.lab, c))
+          if (rgb === null) continue
+
+          return {min: c + 1, max: chromaMax}
+        }
+
+        return {min: chromaMax, max: chromaMax}
+      })
+
+      return badAreas
+    }
+
+    setBadAreas([calculateBadAreasMax(shades)])
   }, [shades, id])
 
   return (
-    <div>
-      {' '}
-      <div className={styles.title}>Chroma</div>
-      <div className={styles.values}>
-        {shades.map(shade => (
-          <div className={styles.value}>
-            {Math.round(calculateChroma(shade.lab))}
-          </div>
-        ))}
-      </div>
-      <div className={styles.chart} id={`chart-chroma-${id}`}></div>
-    </div>
+    <Chart
+      title="Chroma"
+      badAreas={badAreas}
+      convertLabToValue={calculateChroma}
+      id={id}
+      minValue={chromaMin}
+      maxValue={chromaMax}
+      shades={shades}
+      updateValue={(i, value) =>
+        setLab(shades[i].id, applyChromaToLab(shades[i].lab, value))
+      }
+    />
   )
 }
 
--- src/components/charts/HueChart.tsx
+++ src/components/charts/HueChart.tsx
@@ -1,121 +1,46 @@
-import React, {useEffect} from 'react'
-import * as d3 from 'd3'
+import React from 'react'
 
-import {convertLabToHex} from '../../util/color'
 import {Shade, Lab} from '../../types/color'
 
-import styles from './LightnessChart.module.css'
+import Chart from './Chart'
 
-const height = 200
-const width = 400
-
-const hueMax = 0
-const hueMin = 360
+const hueMax = 360
+const hueMin = 0
 const radToDeg = (rad: number) => (rad / Math.PI) * 180 + (rad > 0 ? 0 : 360)
 const calculateHue = ({a, b}: Lab) => radToDeg(Math.atan2(b, a))
+const calculateChroma = ({a, b}: Lab) => Math.sqrt(a * a + b * b)
 
-const draw = (containerId: string, shades: Shade[]) => {
-  let container = document.querySelector(`#chart-hue-${containerId}`)
-  if (container) container.innerHTML = ''
+const applyHueToLab = (lab: Lab, hue: number) => {
+  let chroma = calculateChroma(lab)
 
-  let xScale = d3
-    .scaleLinear()
-    .domain([-0.5, shades.length - 0.5])
-    .range([0, width])
+  let a = chroma * Math.cos((hue / 180) * Math.PI)
+  let b = chroma * Math.sin((hue / 180) * Math.PI)
 
-  let yScale = d3
-    .scaleLinear()
-    .domain([hueMin, hueMax])
-    .range([height, 0])
-
-  let svg = d3
-    .select(`#chart-hue-${containerId}`)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height)
-
-  let defs = svg.append('defs')
-
-  let line = d3
-    .line()
-    .x((d, i) => xScale(i))
-    .y(d => yScale(d[1]))
-    .curve(d3.curveMonotoneX)
-
-  const linearGradient = defs
-    .append('linearGradient')
-    .attr('id', `${containerId}-linear-gradient`)
-    .attr('gradientTrasform', 'rotate(90)')
-  shades.forEach((shade, i) => {
-    linearGradient
-      .append('stop')
-      .attr('offset', `${(100 * i) / shades.length}%`)
-      .attr('stop-color', '#' + convertLabToHex(shade.lab))
-  })
-
-  svg
-    .append('path')
-    .datum(
-      shades.map((shade, i): [number, number] => [i, calculateHue(shade.lab)])
-    )
-    .attr('stroke-width', 4)
-    .attr('fill', 'none')
-    .attr('stroke', `url(#${containerId}-linear-gradient)`)
-    .attr('class', 'line')
-    .attr('d', line)
-
-  svg
-    .selectAll('.dot')
-    .data(
-      shades.map((shade, i): [number, number] => [i, calculateHue(shade.lab)])
-    )
-    .enter()
-    .append('circle')
-    .attr('class', 'dot-border')
-    .attr('fill', d => '#fff')
-    .attr('stroke', d => '#ccc')
-    .attr('cx', (d, i) => xScale(i))
-    .attr('cy', d => yScale(d[1]))
-    .attr('r', 5)
-
-  svg
-    .selectAll('.dot')
-    .data(
-      shades.map((shade, i): [number, number] => [i, calculateHue(shade.lab)])
-    )
-    .enter()
-    .append('circle')
-    .attr('class', 'dot-main')
-    .attr('fill', d => '#' + convertLabToHex(shades[d[0]].lab))
-    .attr('stroke', d => '#fff')
-    .attr('cx', (d, i) => xScale(i))
-    .attr('cy', d => yScale(d[1]))
-    .attr('r', 4)
+  return {...lab, a, b}
 }
 
 interface Props {
   id: string
   shades: Shade[]
+  setLab: (shadeId: string, lab: Lab) => void
 }
 
-const HueChart = ({id, shades}: Props) => {
-  useEffect(() => {
-    draw(id, shades)
-  }, [shades, id])
-
+const LightnessChart = ({id, shades, setLab}: Props) => {
   return (
-    <div>
-      <div className={styles.title}>Hue</div>
-      <div className={styles.values}>
-        {shades.map(shade => (
-          <div className={styles.value}>
-            {Math.round(calculateHue(shade.lab))}
-          </div>
-        ))}
-      </div>
-      <div className={styles.chart} id={`chart-hue-${id}`}></div>
-    </div>
+    <Chart
+      title="Hue"
+      badAreas={[]}
+      convertLabToValue={calculateHue}
+      id={id}
+      minValue={hueMin}
+      maxValue={hueMax}
+      shades={shades}
+      updateValue={(i, value) => {
+        console.log(i, value)
+        setLab(shades[i].id, applyHueToLab(shades[i].lab, value))
+      }}
+    />
   )
 }
 
-export default HueChart
+export default LightnessChart
