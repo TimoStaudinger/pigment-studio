846a00032b2dbd56a60a8a2676b3a0a8b6e7f5fb
Timo Staudinger <mail@timostaudinger.com>
2020-03-29 15:54:54 -0400

    Initial drag & drop support on charts

--- src/components/charts/LightnessChart.tsx
+++ src/components/charts/LightnessChart.tsx
@@ -1,15 +1,20 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import * as d3 from 'd3'
 
 import {convertLabToHex, convertLabToRGBStrict} from '../../util/color'
-import {Shade} from '../../types/color'
+import {Shade, Lab} from '../../types/color'
 
 import styles from './LightnessChart.module.css'
 
 const height = 200
 const width = 400
 
-const draw = (containerId: string, shades: Shade[]) => {
+const draw = (
+  containerId: string,
+  shades: Shade[],
+  setIsInteracting: (isInteracting: boolean) => void,
+  setLightness: (i: number, lightness: number) => void
+) => {
   let container = document.querySelector(`#chart-${containerId}`)
   if (container) container.innerHTML = ''
 
@@ -73,54 +78,6 @@ const draw = (containerId: string, shades: Shade[]) => {
     .attr('transform', 'translate(0,0)')
     .attr('fill', '#c1ccd9')
 
-  svg
-    .append('path')
-    .datum(shades.map((shade, i): [number, number] => [i, shade.lab.l]))
-    .attr('stroke-width', 4)
-    .attr('fill', 'none')
-    .attr('stroke', `url(#${containerId}-linear-gradient)`)
-    .attr('class', 'line')
-    .attr('d', line)
-
-  svg
-    .selectAll('.dot')
-    .data(shades.map((shade, i): [number, number] => [i, shade.lab.l]))
-    .enter()
-    .append('circle')
-    .attr('class', 'dot-border')
-    .attr('fill', d => '#fff')
-    .attr('stroke', d => '#ccc')
-    .attr('cx', (d, i) => xScale(i))
-    .attr('cy', d => yScale(d[1]))
-    .attr('r', 5)
-
-  svg
-    .selectAll('.dot')
-    .data(shades.map((shade, i): [number, number] => [i, shade.lab.l]))
-    .enter()
-    .append('circle')
-    .attr('class', 'dot-main')
-    .attr('fill', d => '#' + convertLabToHex(shades[d[0]].lab))
-    .attr('stroke', d => '#fff')
-    .attr('cx', (d, i) => xScale(i))
-    .attr('cy', d => yScale(d[1]))
-    .attr('r', 4)
-  // .on('mouseover', function(a, b, c) {
-  //   console.log(a)
-  //   this.attr('class', 'focus')
-  // })
-
-  // svg
-  // .append('g')
-  // .attr('class', 'axislabels')
-  // .attr('style', 'transform: translateY(0)')
-  // .call(d3.axisBottom(xScale).tickFormat(d => String(d)))
-  // .call(g => {
-  //   g.selectAll('.domain').remove()
-  //   g.selectAll('.tick line').remove()
-  //   g.selectAll('.tick').attr('style')
-  // })
-
   const calculateBadAreasMin = (shades: Shade[], fixed: 'l' | 'c' | 'h') => {
     let badAreas = shades.map(shade => {
       for (let l = 0; l <= 100; l++) {
@@ -174,17 +131,100 @@ const draw = (containerId: string, shades: Shade[]) => {
 
   drawArea(badAreasMin)
   drawArea(badAreasMax)
+
+  svg
+    .append('path')
+    .datum(shades.map((shade, i): [number, number] => [i, shade.lab.l]))
+    .attr('stroke-width', 4)
+    .attr('fill', 'none')
+    .attr('stroke', `url(#${containerId}-linear-gradient)`)
+    .attr('class', 'line')
+    .attr('d', line)
+
+  svg
+    .selectAll('.dot')
+    .data(shades.map((shade, i): [number, number] => [i, shade.lab.l]))
+    .enter()
+    .append('circle')
+    .attr('class', 'dot-border')
+    .attr('fill', d => '#fff')
+    .attr('stroke', d => '#ccc')
+    .attr('cx', (d, i) => xScale(i))
+    .attr('cy', d => yScale(d[1]))
+    .attr('r', 5)
+
+  svg
+    .selectAll('.dot')
+    .data(shades.map((shade, i): [number, number] => [i, shade.lab.l]))
+    .enter()
+    .append('circle')
+    .attr('class', 'dot-main')
+    .attr('fill', d => '#' + convertLabToHex(shades[d[0]].lab))
+    .attr('stroke', d => '#fff')
+    .attr('cx', (d, i) => xScale(i))
+    .attr('cy', d => yScale(d[1]))
+    .attr('r', 4)
+    .on('mouseenter', (_, i, dots) => d3.select(dots[i]).attr('r', 7))
+    .on('mouseleave', (_, i, dots) => d3.select(dots[i]).attr('r', 4))
+    .call(
+      d3
+        .drag<SVGCircleElement, [number, number]>()
+        .on('start', (_, i, dots) => {
+          setIsInteracting(true)
+          d3.select(dots[i]).attr('fill', 'limegreen')
+        })
+        .on('drag', (d, i, dots) => {
+          const newY = yScale.invert(d3.event.y)
+          // const tickLabel = svg.selectAll('.axislabels text')._groups[0][i]
+          // tickLabel.innerHTML = getTickLabel(d)
+          if (newY > 100 || newY < 0) return
+
+          const dot = dots[i]
+          d3.select(dot).attr('cy', yScale(newY))
+
+          // @ts-ignore
+          const dotBorder = svg.selectAll('.dot-border')._groups[0][i]
+          d3.select(dotBorder).attr('cy', yScale(newY))
+
+          setLightness(i, yScale.invert(d3.event.y))
+
+          // d.x = newY
+          svg
+            .select('.line')
+            .datum(shades.map((shade, i): [number, number] => [i, shade.lab.l]))
+            .attr('d', line)
+        })
+        .on('end', () => setIsInteracting(false))
+    )
+
+  // svg
+  // .append('g')
+  // .attr('class', 'axislabels')
+  // .attr('style', 'transform: translateY(0)')
+  // .call(d3.axisBottom(xScale).tickFormat(d => String(d)))
+  // .call(g => {
+  //   g.selectAll('.domain').remove()
+  //   g.selectAll('.tick line').remove()
+  //   g.selectAll('.tick').attr('style')
+  // })
 }
 
 interface Props {
   id: string
   shades: Shade[]
+  setLab: (shadeId: string, lab: Lab) => void
 }
 
-const LightnessChart = ({id, shades}: Props) => {
+const LightnessChart = ({id, shades, setLab}: Props) => {
+  let [isInteracting, setIsInteracting] = useState(false)
+
+  const setLightness = (i: number, lightness: number) => {
+    setLab(shades[i].id, {...shades[i].lab, l: lightness})
+  }
+
   useEffect(() => {
-    draw(id, shades)
-  }, [shades, id])
+    if (!isInteracting) draw(id, shades, setIsInteracting, setLightness)
+  }, [shades, id, isInteracting])
 
   return (
     <div>
