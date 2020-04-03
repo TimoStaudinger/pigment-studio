aa85f499efef3f4293a45357652188ae6e470b14
Timo Staudinger <mail@timostaudinger.com>
2020-04-03 18:50:25 -0400

    Use reach ui tab component for better a11y

--- packages/app/src/components/workbench/Workbench.tsx
+++ packages/app/src/components/workbench/Workbench.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import classnames from 'classnames'
 import {useParams, useHistory, generatePath} from 'react-router-dom'
+import {Tabs, TabList, Tab, TabPanels, TabPanel} from '@reach/tabs'
 
 import Button from '../common/Button'
 
+import '@reach/tabs/styles.css'
 import styles from './Workbench.module.css'
 
 interface Props {
@@ -14,33 +16,32 @@ const Workbench = ({areas}: Props) => {
   let {view, ...params} = useParams()
   let history = useHistory()
 
-  let selectedView = view ?? areas?.[0]?.[0]
+  // let selectedView = view ?? areas?.[0]?.[0]
 
   return (
-    <div className={styles.container}>
-      <div className={styles.tabbar}>
-        {areas.map((area) => (
-          <Button
-            className={classnames(styles.tabbarItem, {
-              [styles.selected]: selectedView === area[0]
-            })}
-            onClick={() =>
-              history.push(
-                generatePath('/:paletteId?/:colorIndex?/:shadeIndex?/:view?', {
-                  ...params,
-                  view: area[0]
-                })
-              )
-            }
-            text={area[0]}
-          />
+    <Tabs
+      className={styles.container}
+      index={(view && parseInt(view, 10)) || 0}
+      onChange={(view) =>
+        history.push(
+          generatePath('/:paletteId?/:colorIndex?/:shadeIndex?/:view?', {
+            ...params,
+            view
+          })
+        )
+      }
+    >
+      <TabList className={styles.tabbar}>
+        {areas.map(([name]) => (
+          <Tab key={name}>{name}</Tab>
         ))}
-      </div>
-
-      <div className={styles.content}>
-        {areas ? areas.find((area) => area[0] === selectedView)?.[1] : null}
-      </div>
-    </div>
+      </TabList>
+      <TabPanels className={styles.content}>
+        {areas.map(([name, content]) => (
+          <TabPanel key={name}>{content}</TabPanel>
+        ))}
+      </TabPanels>{' '}
+    </Tabs>
   )
 }
 export default Workbench
